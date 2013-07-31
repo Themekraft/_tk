574688c53d6b5fa9983881147a26f09a625e1429
themekraft <team@themekraft.com>
2013-07-31 22:00:57 +0200

    last changes for container padding and content width

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) )
-	$content_width = 640; /* pixels */
+	$content_width = 750; /* pixels */
 
 if ( ! function_exists( '_tk_setup' ) ) :
 /**
