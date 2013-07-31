83fd1e6a44a4cef9d4436d537bf048086a6c31e2
themekraft <team@themekraft.com>
2013-07-31 21:49:40 +0200

    little fixes

--- header.php
+++ header.php
@@ -35,7 +35,7 @@
 				
 				<div class="site-branding">
 					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+					<h4 class="site-description"><?php bloginfo( 'description' ); ?></h4>
 				</div>
 						
 			</div>
