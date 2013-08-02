8eef25a71e3ba1a65217bf78256f5d1d6ca0cdd6
themekraft <team@themekraft.com>
2013-08-02 12:28:13 +0200

    bootstrap WP JS additions

---
+++ js/bootstrapwp.js
@@ -0,0 +1,28 @@
+// Comment section additions to add classes for bootstrap styles 
+jQuery(document).ready(function(){
+
+	// here for each comment reply link of wordpress
+	jQuery( '.comment-reply-link' ).addClass( 'btn btn-primary' );
+
+	// here for the submit button of the comment reply form
+	jQuery( '#commentsubmit' ).addClass( 'btn btn-primary' );
+	
+	
+	// The WordPress Default Widgets 
+	// Now we'll add some classes for the wordpress default widgets - let's go  
+	
+	// the search widget	
+	jQuery( 'input.search-field' ).addClass( 'form-control' );
+	jQuery( 'input.search-submit' ).addClass( 'btn btn-default' );
+	
+	jQuery( '.widget_meta ul, .widget_recent_entries ul, .widget_archive ul, .widget_categories ul, .widget_nav_menu ul' ).addClass( 'nav' );
+
+	jQuery( '.widget_recent_comments ul#recentcomments' ).css( 'list-style', 'none').css( 'padding-left', '0' );
+	jQuery( '.widget_recent_comments ul#recentcomments li' ).css( 'padding', '5px 15px');
+			
+		
+
+	
+});
+
+
