477fbfca968fde479f9f2f962d8f8dbfd90bf904
themekraft <team@themekraft.com>
2013-08-02 12:27:28 +0200

    add glyphicons

--- functions.php
+++ functions.php
@@ -97,15 +97,18 @@ function _tk_scripts() {
 	wp_enqueue_style( '_tk-style', get_stylesheet_uri() );
 
 	wp_enqueue_style( '_tk-bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css' );
+	
+	wp_enqueue_style( '_tk-glyphicons', get_template_directory_uri() . '/glyphicons/css/bootstrap-glyphicons.css' );
 
 	wp_enqueue_script('_tk-bootstrapjs', get_template_directory_uri().'/bootstrap/js/bootstrap.min.js', array('jquery') );
 
 	wp_enqueue_script( '_tk-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( '_tk-bootstrapwp', get_template_directory_uri() . '/js/bootstrapwp.js', array('jquery') );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
+	
 	if ( is_singular() && wp_attachment_is_image() ) {
 		wp_enqueue_script( '_tk-keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
