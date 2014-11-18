e019b3f804d3df81fb1fd4fa7325c4bcf7abfb16
Ryan Macdonald <macdonaldr93@gmail.com>
2014-11-18 16:16:23 -0500

    add fix for old IE browsers

--- header.php
+++ header.php
@@ -9,12 +9,16 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<?php wp_head(); ?>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
