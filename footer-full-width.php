8172a0b246df922938900f73208b1e9f9056939a
Sameer Shelavale <sameer@gohachi.com>
2016-02-27 19:55:58 +0530

    added full width layout

---
+++ footer-full-width.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package _tk
+ */
+?>
+			</div><!-- close .*-inner (main-content or sidebar, depending if sidebar is used) -->
+		</div><!-- close .row -->
+	</div><!-- close .container -->
+</div><!-- close .main-content -->
+
+<footer id="colophon" class="site-footer" role="contentinfo">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 col-sm-12">
+                <!-- footer start-->
+                <?php dynamic_sidebar( 'footer' ); ?>
+                <!-- footer end -->
+            </div>
+        </div>
+		<div class="row">
+			<div class="site-footer-inner col-sm-12">
+
+				<div class="site-info">
+					<?php do_action( '_tk_credits' ); ?>
+					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_tk' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_tk' ), 'WordPress' ); ?></a>
+					<span class="sep"> | </span>
+                    <a class="credits" href="http://themekraft.com/" target="_blank" title="Themes and Plugins developed by Themekraft" alt="Themes and Plugins developed by Themekraft"><?php _e('Themes and Plugins developed by Themekraft.','_tk') ?> </a>
+				</div><!-- close .site-info -->
+
+			</div>
+		</div>
+	</div><!-- close .container -->
+</footer><!-- close #colophon -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ header-full-width.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="main">
+ *
+ * @package _tk
+ */
+?><!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php do_action( 'before' ); ?>
+
+<header id="masthead" class="site-header" role="banner">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="site-header-inner col-sm-12">
+
+				<?php $header_image = get_header_image();
+				if ( ! empty( $header_image ) ) { ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+						<img src="<?php header_image(); ?>" width="<?php echo get_custom_header()->width; ?>" height="<?php echo get_custom_header()->height; ?>" alt="">
+					</a>
+				<?php } // end if ( ! empty( $header_image ) ) ?>
+
+
+				<div class="site-branding">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<p class="lead"><?php bloginfo( 'description' ); ?></p>
+				</div>
+
+			</div>
+		</div>
+	</div><!-- .container -->
+</header><!-- #masthead -->
+
+<nav class="site-navigation">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="site-navigation-inner col-sm-12">
+				<div class="navbar navbar-default">
+					<div class="navbar-header">
+						<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+							<span class="sr-only"><?php _e('Toggle navigation','_tk') ?> </span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+	
+						<!-- Your site title as branding in the menu -->
+						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+					</div>
+
+					<!-- The WordPress Menu goes here -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location' 	=> 'primary',
+							'depth'             => 2,
+							'container'         => 'div',
+							'container_class'   => 'collapse navbar-collapse',
+							'menu_class' 		=> 'nav navbar-nav',
+							'fallback_cb' 		=> 'wp_bootstrap_navwalker::fallback',
+							'menu_id'			=> 'main-menu',
+							'walker' 			=> new wp_bootstrap_navwalker()
+						)
+					); ?>
+
+				</div><!-- .navbar -->
+			</div>
+		</div>
+	</div><!-- .container -->
+</nav><!-- .site-navigation -->
+
+<div class="main-content">
+<?php // substitute the class "container-fluid" below if you want a wider content area ?>
+	<div class="container-fluid">
+		<div class="row">
+			<div id="content" class="main-content-inner col-sm-12 col-md-8">
+
