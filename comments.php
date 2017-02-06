cb49623059be874a0e3a578520b99dd9d66b0085
Kamil Breguła <mik-laj@users.noreply.github.com>
2017-02-06 01:10:43 +0100

    Add a translation support for comment's placeholer

--- comments.php
+++ comments.php
@@ -80,7 +80,7 @@ if ( post_password_required() )
 			  'cancel_reply_link' => __( 'Cancel Reply', '_tk' ),  // that's the wordpress default value! delete it or edit it ;)
 			  'label_submit'      => __( 'Post Comment', '_tk' ),  // that's the wordpress default value! delete it or edit it ;)
 
-			  'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+			  'comment_field' =>  '<p><textarea placeholder="' . __('Start typing...', '_tk') . '" id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
 
 			  'comment_notes_after' => '<p class="form-allowed-tags">' .
 				__( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:', '_tk' ) .
